7cc08392d9ffc31747e3558566d7cc72857a657e
Divya-1200 <divya07msd@example.com>
2021-04-10 21:16:01 +0530

    api test cases added

--- app.py
+++ app.py
@@ -23,6 +23,24 @@ def startpy():
 
     return render_template("index.html", result = result)
 
+'''
+http://0.0.0.0:3091/api/data
+'''
+
+@app.route("/api/data", methods=["GET"])
+def api_get_data():
+
+    ontario_data = [20113, 21019, 22037, 22844, 23903]
+
+    quebec_data = [10000, 11000, 6542, 8907, 6782]
+
+    result_dict = {
+        'ontario' : ontario_data,
+        'quebec'  : quebec_data
+    }
+
+    return result_dict
+
 
 if __name__ == "__main__":
     app.run( debug = True,host="0.0.0.0",port = PORT)
